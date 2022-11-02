13027571e5855d0840fa8f9d2f76b0bec5412c60
Shafiyah Huyai <63112523+shafiyahhuyai@users.noreply.github.com>
2022-11-02 21:50:46 +0700

    Update index.js

---
+++ src/learn-useContext/index.js
@@ -0,0 +1,18 @@
+const dataContext = [
+   {
+    nama: "Shafiyah Huyai",
+    link: "https://www.instagram.com/huyaiiou/",
+  },
+  {
+    nama: "Alif Nabil Musyafa",
+    link: "",
+  },
+  {
+    nama: "Yusuf Hammadi",
+    link: "",
+  },
+  {
+    nama: "M. Dzikrullah Farhan",
+    link: "",
+  },
+];
